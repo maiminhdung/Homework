f556fd0545419170b4f024ef007107595844645c
Dungjava <99890979+maiminhdung@users.noreply.github.com>
2025-03-16 22:13:20 +0700

    Recode bai_1 to bai_10

---
+++ Codedream/bai_4.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a * 7 + b * 7;
+    return 0;
+}
---
+++ Codedream/bai_5.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x, y;
+    cin >> n >> x >> y;
+    cout << 3 * n + 2 * x - y;
+}
